6e61142a96c27735016ead02562b46179b507cc3
Ubuntu <kewei.xie@outlook.com>
2024-01-13 03:56:56 +0000

    fix

--- jobs/run_news_push_blockbeats.sh
+++ jobs/run_news_push_blockbeats.sh
@@ -1,4 +1,12 @@
 
 
 
-/home/ubuntu/miniconda3/envs/news-push/bin/python -u news_push_blockbeats.py >>log.python.news_push_blockbeats 2>&1
+ts=$(date +%s)
+if ( ps aux | grep "python news_push_blockbeats.py" | grep grep -v >/dev/null 2>&1 ); then
+    echo $ts [$(date)] "Processing ..."
+else
+    echo $ts [$(date)] "Start ..."
+    /home/ubuntu/miniconda3/envs/news-push/bin/python -u news_push_blockbeats.py >>log.python.news_push_blockbeats 2>&1
+    echo $ts [$(date)] "End !!!" 
+fi
+
